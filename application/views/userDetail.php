3e64f62bb2464fc947f7ce62825989b3878a2971
ztao <mail@vekwu.com>
2016-08-13 19:55:40 +0800

    合并前端基本完成:dog:

--- application/views/userDetail.php
+++ application/views/userDetail.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">  
 	<title>学长学姐帮帮忙</title>
-	<link rel="stylesheet" href="<?php echo base_url('static/css/userH.debug.css');?>">
+	<link rel="stylesheet" href="<?php echo base_url('static/css/user.debug.css');?>">
 	<style>
 		@media screen and (min-width:320px) {
 		    html {
